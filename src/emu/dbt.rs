nbdd0121/r2vm

commit c3d7560cde63918a3c50f890da4491e6de736b52

Conditional execution optimisation

src/emu/dbt.rs
@@ -1766,6 +1766,81 @@ impl<'a> DbtCompiler<'a> {
         self.instret += 1;
     }
 
+    /// Compile an conditional execution instruction pair.
+    /// The first op in the pair should follow the previous one fed to this compiler, and it must
+    /// be a branching instruction.
+    ///
+    /// *Note*: Unlike `compile_op`, this function will not respect sanitize feature. If these are needed, macro-op fusion should better be disable at all.
+    /// It should also be noted that there will be no interrupt check in the generated code, making
+    /// it slightly different in behaviour. However it is guaranteed that the number of cycles
+    /// taken is the same.
+    ///
+    /// *TODO*: In the future we may want to support multiple jump-ahead if everything is contained
+    /// within an extended basic block, but to allow for conditional load/move, this is an easy
+    /// optimisation.
+    pub fn compile_cond_op(&mut self, bop: &Op, bcomp: bool, op: &Op, comp: bool) {
+        let blen = if bcomp { 2 } else { 4 };
+        let olen = if comp { 2 } else { 4 };
+
+        // First check if the op cross cache block boundary and we need to access the icache.
+        self.pc_end = self.pc_cur + blen + olen;
+        if (self.pc_start.wrapping_add(self.pc_cur as u64) - 1) >> CACHE_LINE_LOG2_SIZE != (self.pc_start.wrapping_add(self.pc_start as u64) - 1) >> CACHE_LINE_LOG2_SIZE {
+            self.emit_icache_access(self.pc_end - 1, false);
+        }
+
+        // Get the operands of the branch
+        let (rs1, rs2, mut cc) = match bop {
+            &Op::Beq { rs1, rs2, .. } => (rs1, rs2, ConditionCode::Equal),
+            &Op::Bne { rs1, rs2, .. } => (rs1, rs2, ConditionCode::NotEqual),
+            &Op::Blt { rs1, rs2, .. } => (rs1, rs2, ConditionCode::Less),
+            &Op::Bge { rs1, rs2, .. } => (rs1, rs2, ConditionCode::GreaterEqual),
+            &Op::Bltu { rs1, rs2, .. } => (rs1, rs2, ConditionCode::Below),
+            &Op::Bgeu { rs1, rs2, .. } => (rs1, rs2, ConditionCode::AboveEqual),
+            _ => unreachable!(),
+        };
+
+        // In non-threaded mode, generate a yield call.
+        // This is done before we start the sequence as we cannot do it between the macro-op.
+        if !crate::threaded() {
+            self.emit_helper_call(fiber_yield_raw);
+        }
+
+        // Compare and set flags.
+        // If either operand is 0, it should be treated specially.
+        if rs2 == 0 {
+            self.emit(Cmp(loc_of_register(rs1), Imm(0)));
+        } else if rs1 == 0 {
+            // Switch around condition code in this case.
+            cc = cc.swap();
+            self.emit(Cmp(loc_of_register(rs2), Imm(0)));
+        } else {
+            self.emit(Mov(Reg(Register::RDX), loc_of_register(rs1).into()));
+            self.emit(Cmp(Reg(Register::RDX), loc_of_register(rs2).into()));
+        }
+
+        let jcc = self.emit_jcc_long(cc);
+
+        // Increment instret here eagerly, because we need to track precise instret.
+        // So we must differentiate between two paths.
+        self.emit(Add(memory_of!(instret).into(), Imm(1)));
+
+        // Emit the conditionally executed op.
+        self.pc_cur += blen as i64;
+        self.emit_op(op, comp);
+
+        // In non-threaded mode, generate a yield call.
+        if !crate::threaded() {
+            self.emit_helper_call(fiber_yield_raw);
+        }
+
+        let label_fin = self.label();
+        self.patch(jcc, label_fin);
+
+        // Advance counters past both instructions
+        self.pc_cur = self.pc_end;
+        self.instret += 1;
+    }
+
     pub fn begin(&mut self, pc: u64) {
         self.pc_start = pc;
 

src/emu/interp.rs
@@ -1729,6 +1729,58 @@ fn translate_code(icache: &mut ICache, prv: u64, phys_pc: u64) -> (usize, usize)
         // We must not emit code for protected ops
         if (prv as u8) < op.min_prv_level() { op = Op::Illegal }
 
+        // The way we generate code is a bit slow for branches. The mini-optimisation here
+        // captures conditional execution patterns.
+        match op {
+            Op::Beq { imm: 6, .. } |
+            Op::Bne { imm: 6, .. } |
+            Op::Blt { imm: 6, .. } |
+            Op::Bge { imm: 6, .. } |
+            Op::Bltu { imm: 6, .. } |
+            Op::Bgeu { imm: 6, .. } => {
+                if let Ok((next_op, true, bits)) = read_insn(phys_pc_end as usize) {
+                    // Currently we require the conditional executed instruction to not change
+                    // control flow.
+                    if !next_op.can_change_control_flow() {
+                        if crate::get_flags().disassemble {
+                            eprintln!("{}", next_op.pretty_print(phys_pc_end, bits));
+                        }
+                        phys_pc_end += 2;
+                        compiler.compile_cond_op(&op, c, &next_op, true);
+                        if phys_pc_end & 4095 == 0 {
+                            compiler.end();
+                            break
+                        }
+                        continue;
+                    }
+                }
+            }
+            Op::Beq { imm: 8, .. } |
+            Op::Bne { imm: 8, .. } |
+            Op::Blt { imm: 8, .. } |
+            Op::Bge { imm: 8, .. } |
+            Op::Bltu { imm: 8, .. } |
+            Op::Bgeu { imm: 8, .. } => {
+                if let Ok((next_op, false, bits)) = read_insn(phys_pc_end as usize) {
+                    // Currently we require the conditional executed instruction to not change
+                    // control flow.
+                    if !next_op.can_change_control_flow() {
+                        if crate::get_flags().disassemble {
+                            eprintln!("{}", next_op.pretty_print(phys_pc_end, bits));
+                        }
+                        phys_pc_end += 4;
+                        compiler.compile_cond_op(&op, c, &next_op, false);
+                        if phys_pc_end & 4095 == 0 {
+                            compiler.end();
+                            break
+                        }
+                        continue;
+                    }
+                }
+            }
+            _ => (),
+        }
+
         match op {
             Op::Beq { .. } |
             Op::Bne { .. } |

commit 96849132494100c61bdfa367562c1d7c4e926158

Fix bug introduced in c3d7560
We need to make sure that in absolutely **no** cases we access memory
across page boundaries.

src/emu/dbt.rs
@@ -2035,7 +2035,7 @@ impl<'a> DbtCompiler<'a> {
         self.pc_end = self.pc_cur + blen + olen;
         if !same_cache_line(
             self.pc_start.wrapping_add(self.pc_cur as u64) - 1,
-            self.pc_start.wrapping_add(self.pc_start as u64) - 1,
+            self.pc_start.wrapping_add(self.pc_end as u64) - 1,
         ) {
             self.emit_icache_access(self.pc_end - 1, false);
         }

src/emu/interp.rs
@@ -1787,50 +1787,56 @@ fn translate_code(icache: &mut ICache, prv: u64, phys_pc: u64) -> (usize, usize)
 
         // The way we generate code is a bit slow for branches. The mini-optimisation here
         // captures conditional execution patterns.
-        match op {
-            Op::Beq { imm: 6, .. }
-            | Op::Bne { imm: 6, .. }
-            | Op::Blt { imm: 6, .. }
-            | Op::Bge { imm: 6, .. }
-            | Op::Bltu { imm: 6, .. }
-            | Op::Bgeu { imm: 6, .. } => {
-                if let Ok((next_op, true, bits)) = read_insn(phys_pc_end as usize) {
-                    // Currently we require the conditional executed instruction to not change
-                    // control flow.
-                    if crate::get_flags().disassemble {
-                        eprintln!("{}", next_op.pretty_print(phys_pc_end, bits));
+        // Note that this shouldn't be done if the fused macro-op can cross page boundary.
+        if phys_pc_end & 4095 != 0 {
+            match op {
+                Op::Beq { imm: 6, .. }
+                | Op::Bne { imm: 6, .. }
+                | Op::Blt { imm: 6, .. }
+                | Op::Bge { imm: 6, .. }
+                | Op::Bltu { imm: 6, .. }
+                | Op::Bgeu { imm: 6, .. } => {
+                    if let Ok((next_op, true, bits)) = read_insn(phys_pc_end as usize) {
+                        if phys_pc_end & 4095 == 0 {
+                            error!("OOOPS");
+                        }
+                        // Currently we require the conditional executed instruction to not change
+                        // control flow.
+                        if crate::get_flags().disassemble {
+                            eprintln!("{}", next_op.pretty_print(phys_pc_end, bits));
+                        }
+                        phys_pc_end += 2;
+                        compiler.compile_cond_op(&op, c, &next_op, true);
+                        if phys_pc_end & 4095 == 0 {
+                            compiler.end();
+                            break;
+                        }
+                        continue;
                     }
-                    phys_pc_end += 2;
-                    compiler.compile_cond_op(&op, c, &next_op, true);
-                    if phys_pc_end & 4095 == 0 {
-                        compiler.end();
-                        break;
-                    }
-                    continue;
                 }
-            }
-            Op::Beq { imm: 8, .. }
-            | Op::Bne { imm: 8, .. }
-            | Op::Blt { imm: 8, .. }
-            | Op::Bge { imm: 8, .. }
-            | Op::Bltu { imm: 8, .. }
-            | Op::Bgeu { imm: 8, .. } => {
-                if let Ok((next_op, false, bits)) = read_insn(phys_pc_end as usize) {
-                    // Currently we require the conditional executed instruction to not change
-                    // control flow.
-                    if crate::get_flags().disassemble {
-                        eprintln!("{}", next_op.pretty_print(phys_pc_end, bits));
+                Op::Beq { imm: 8, .. }
+                | Op::Bne { imm: 8, .. }
+                | Op::Blt { imm: 8, .. }
+                | Op::Bge { imm: 8, .. }
+                | Op::Bltu { imm: 8, .. }
+                | Op::Bgeu { imm: 8, .. } => {
+                    if let Ok((next_op, false, bits)) = read_insn(phys_pc_end as usize) {
+                        // Currently we require the conditional executed instruction to not change
+                        // control flow.
+                        if crate::get_flags().disassemble {
+                            eprintln!("{}", next_op.pretty_print(phys_pc_end, bits));
+                        }
+                        phys_pc_end += 4;
+                        compiler.compile_cond_op(&op, c, &next_op, false);
+                        if phys_pc_end & 4095 == 0 {
+                            compiler.end();
+                            break;
+                        }
+                        continue;
                     }
-                    phys_pc_end += 4;
-                    compiler.compile_cond_op(&op, c, &next_op, false);
-                    if phys_pc_end & 4095 == 0 {
-                        compiler.end();
-                        break;
-                    }
-                    continue;
                 }
+                _ => (),
             }
-            _ => (),
         }
 
         match op {
@@ -1859,6 +1865,9 @@ fn translate_code(icache: &mut ICache, prv: u64, phys_pc: u64) -> (usize, usize)
             break;
         }
     }
+
+    // We must not cross page boundary in absolutely any cases.
+    assert_eq!(phys_pc & !4095, (phys_pc_end - 1) & !4095, "op crosses page boundary");
 
     let func_len = compiler.len;
     assert!(func_len <= 256 * 1024);