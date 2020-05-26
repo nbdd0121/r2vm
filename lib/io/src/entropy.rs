6f86196f781acbf259f75b307e4af34224d70664
Gary Guo <gary@garyguo.net>
2020-05-26 22:53:39 +0100

    Switch StdRng to ChaCha8Rng
    This ensures the random seed is reproducible even after `rand` switches
    its default implementation.

--- lib/io/src/entropy.rs
+++ lib/io/src/entropy.rs
@@ -8,6 +8,6 @@ pub use rand;
 #[doc(no_inline)]
 pub use rand::rngs::OsRng as Os;
 #[doc(no_inline)]
-pub use rand::rngs::StdRng as Seeded;
-#[doc(no_inline)]
 pub use rand::RngCore as Entropy;
+#[doc(no_inline)]
+pub use rand_chacha::ChaCha8Rng as Seeded;
